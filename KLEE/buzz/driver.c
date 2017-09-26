4686a1ebb4b74d79a3ec19edc5ea5673a522194a
Liujia Hu <skydamon@outlook.com>
2017-09-26 20:55:43 +0800

    driver.c

--- KLEE/buzz/driver.c
+++ KLEE/buzz/driver.c
@@ -34,25 +34,11 @@ int main(int argc, char *argv[]){
 	memcpy(&pkt4.packet.tcpSYN, &syn_of_pkt4, sizeof(syn_of_pkt4));
 	memcpy(&pkt5.packet.tcpSYN, &syn_of_pkt5, sizeof(syn_of_pkt5));
 
-
-
-	int sig_of_pkt1;
-	int sig_of_pkt2;
-	int sig_of_pkt3;
-	int sig_of_pkt4;
-    int sig_of_pkt5;
-
-	klee_make_symbolic(&sig_of_pkt1, sizeof(sig_of_pkt1), "pkt1.packet.signature");
-	klee_make_symbolic(&sig_of_pkt2, sizeof(sig_of_pkt2), "pkt2.packet.signature");
-	klee_make_symbolic(&sig_of_pkt3, sizeof(sig_of_pkt3), "pkt3.packet.signature");
-	klee_make_symbolic(&sig_of_pkt4, sizeof(sig_of_pkt4), "pkt4.packet.signature");
-    klee_make_symbolic(&sig_of_pkt5, sizeof(sig_of_pkt5), "pkt5.packet.signature");
-
-	memcpy(&pkt1.packet.signature, &sig_of_pkt1, sizeof(sig_of_pkt1));
-	memcpy(&pkt2.packet.signature, &sig_of_pkt2, sizeof(sig_of_pkt2));
-	memcpy(&pkt3.packet.signature, &sig_of_pkt3, sizeof(sig_of_pkt3));
-	memcpy(&pkt4.packet.signature, &sig_of_pkt4, sizeof(sig_of_pkt4));
-    memcpy(&pkt5.packet.signature, &sig_of_pkt5, sizeof(sig_of_pkt5));
+	klee_make_symbolic(&pkt1.packet.signature, sizeof(pkt1.packet.signature), "pkt1.packet.signature");
+	klee_make_symbolic(&pkt2.packet.signature, sizeof(pkt2.packet.signature), "pkt2.packet.signature");
+	klee_make_symbolic(&pkt3.packet.signature, sizeof(pkt3.packet.signature), "pkt3.packet.signature");
+	klee_make_symbolic(&pkt4.packet.signature, sizeof(pkt4.packet.signature), "pkt4.packet.signature");
+    klee_make_symbolic(&pkt5.packet.signature, sizeof(pkt1.packet.signature), "pkt5.packet.signature");
 	
     //klee_assert(hips_badsig !=1);
 	return 0;
