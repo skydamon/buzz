90baa091ec4ec63442c01be3325a1daa21aa1d22
Damon <skydamon@outlook.com>
2017-09-28 12:03:50 +0000

    driver.c

--- KLEE/buzz/driver.c
+++ KLEE/buzz/driver.c
@@ -762,85 +762,91 @@ int main(int argc, char *argv[]){
 
 	int zz;
 
-    locatedPacket pkt1;
-    pkt1.packet.id = 1;
-    pkt1.packet.srcIP = 0;
-//  pkt1.packet.dstIP = 2;
-    pkt1.packet.dropped = 0;
-    pkt1.packet.tag = 0;
-    pkt1.packet.isHttp = 0;
-    pkt1.packet.timeout = 0;
-    pkt1.port.num = 0;
+	locatedPacket pkt1;
+	pkt1.packet.id = 1;
+	pkt1.packet.srcIP = 0;
+	pkt1.packet.dstIP = 1;
+	pkt1.packet.dropped = 0;
+	pkt1.packet.tag = 0;
+	pkt1.packet.isHttp = 0;
+	pkt1.packet.timeout = 0;
+	pkt1.port.num = 0;
 
-    locatedPacket pkt2;
-    pkt2.packet.id = 2;
-    pkt2.packet.srcIP = 0;
-//  pkt2.packet.dstIP = 2;
-    pkt2.packet.dropped = 0;
-    pkt2.packet.tag = 0;
-    pkt2.packet.isHttp = 0;
-    pkt2.packet.timeout = 0;
-    pkt2.port.num = 0;
+	locatedPacket pkt2;
+	pkt2.packet.id = 2;
+	pkt2.packet.srcIP = 0;
+	pkt2.packet.dstIP = 1;
+	pkt2.packet.dropped = 0;
+	pkt2.packet.tag = 0;
+	pkt2.packet.isHttp = 0;
+	pkt2.packet.timeout = 0;
+	pkt2.port.num = 0;
 
-    locatedPacket pkt3;
-    pkt3.packet.id = 3;
-    pkt3.packet.srcIP = 0;
-//  pkt3.packet.dstIP = 2;
-    pkt3.packet.dropped = 0;
-    pkt3.packet.tag = 0;
-    pkt3.packet.isHttp = 0;
-    pkt3.packet.timeout = 0;
-    pkt3.port.num = 0;
+	locatedPacket pkt3;
+	pkt3.packet.id = 3;
+	pkt3.packet.srcIP = 0;
+	pkt3.packet.dstIP = 1;
+	pkt3.packet.dropped = 0;
+	pkt3.packet.tag = 0;
+	pkt3.packet.isHttp = 0;
+	pkt3.packet.timeout = 0;
+	pkt3.port.num = 0;
 
-    locatedPacket pkt4;
-    pkt4.packet.id = 4;
-    pkt4.packet.srcIP = 0;
-//  pkt4.packet.dstIP = 2;
-    pkt4.packet.dropped = 0;
-    pkt4.packet.tag = 0;
-    pkt4.packet.isHttp = 0;
-    pkt4.packet.timeout = 0;
-    pkt4.port.num = 0;
+	locatedPacket pkt4;
+	pkt4.packet.id = 4;
+	pkt4.packet.srcIP = 0;
+	pkt4.packet.dstIP = 1;
+	pkt4.packet.dropped = 0;
+	pkt4.packet.tag = 0;
+	pkt4.packet.isHttp = 0;
+	pkt4.packet.timeout = 0;
+	pkt4.port.num = 0;
 
-    locatedPacket pkt5;
-    pkt5.packet.id = 5;
-    pkt5.packet.srcIP = 0;
-//  pkt4.packet.dstIP = 2;
-    pkt5.packet.dropped = 0;
-    pkt5.packet.tag = 0;
-    pkt5.packet.isHttp = 0;
-    pkt5.packet.timeout = 0;
-    pkt5.port.num = 0;
 
-    int syn_of_pkt1;
-    int syn_of_pkt2;
-    int syn_of_pkt3;
-    int syn_of_pkt4;
-    int syn_of_pkt5;
+	pkt1.packet.tcpSYN = 1;
+	pkt2.packet.tcpSYN = 1;
+	pkt3.packet.tcpSYN = 1;
+	pkt4.packet.tcpSYN = 1;
 
-    IP dstIP_of_pkt1;
-    IP dstIP_of_pkt2;
-    IP dstIP_of_pkt3;
-    IP dstIP_of_pkt4;
-    IP dstIP_of_pkt5;
+	int syn_of_pkt1;
+	int syn_of_pkt2;
+	int syn_of_pkt3;
+	int syn_of_pkt4;
 
-    klee_make_symbolic(&syn_of_pkt1, sizeof(syn_of_pkt1), "pkt1.packet.tcpSYN");
-    klee_make_symbolic(&syn_of_pkt2, sizeof(syn_of_pkt2), "pkt2.packet.tcpSYN");
-    klee_make_symbolic(&syn_of_pkt3, sizeof(syn_of_pkt3), "pkt3.packet.tcpSYN");
-    klee_make_symbolic(&syn_of_pkt4, sizeof(syn_of_pkt4), "pkt4.packet.tcpSYN");
-    klee_make_symbolic(&syn_of_pkt5, sizeof(syn_of_pkt5), "pkt5.packet.tcpSYN");
+	IP dstIP_of_pkt1;
+	IP dstIP_of_pkt2;
+	IP dstIP_of_pkt3;
+	IP dstIP_of_pkt4;
 
-    klee_make_symbolic(&dstIP_of_pkt1, sizeof(dstIP_of_pkt1), "pkt1.packet.dstIP");
-    klee_make_symbolic(&dstIP_of_pkt2, sizeof(dstIP_of_pkt2), "pkt2.packet.dstIP");
-    klee_make_symbolic(&dstIP_of_pkt3, sizeof(dstIP_of_pkt3), "pkt3.packet.dstIP");
-    klee_make_symbolic(&dstIP_of_pkt4, sizeof(dstIP_of_pkt4), "pkt4.packet.dstIP");
-    klee_make_symbolic(&dstIP_of_pkt5, sizeof(dstIP_of_pkt5), "pkt5.packet.dstIP");
-    
-    memcpy(&pkt1.packet.dstIP, &dstIP_of_pkt1, sizeof(IP));
-    memcpy(&pkt2.packet.dstIP, &dstIP_of_pkt2, sizeof(IP));
-    memcpy(&pkt3.packet.dstIP, &dstIP_of_pkt3, sizeof(IP));
-    memcpy(&pkt4.packet.dstIP, &dstIP_of_pkt4, sizeof(IP));
-    memcpy(&pkt5.packet.dstIP, &dstIP_of_pkt5, sizeof(IP));
+	klee_make_symbolic(&syn_of_pkt1, sizeof(syn_of_pkt1), "pkt1.packet.tcpSYN");
+	klee_make_symbolic(&syn_of_pkt2, sizeof(syn_of_pkt2), "pkt2.packet.tcpSYN");
+	klee_make_symbolic(&syn_of_pkt3, sizeof(syn_of_pkt3), "pkt3.packet.tcpSYN");
+	klee_make_symbolic(&syn_of_pkt4, sizeof(syn_of_pkt4), "pkt4.packet.tcpSYN");
+
+	memcpy(&pkt1.packet.tcpSYN, &syn_of_pkt1, sizeof(syn_of_pkt1));
+	memcpy(&pkt2.packet.tcpSYN, &syn_of_pkt2, sizeof(syn_of_pkt2));
+	memcpy(&pkt3.packet.tcpSYN, &syn_of_pkt3, sizeof(syn_of_pkt3));
+	memcpy(&pkt4.packet.tcpSYN, &syn_of_pkt4, sizeof(syn_of_pkt4));
+	
+	pkt1.packet.signature = 1;
+	pkt2.packet.signature = 1;
+	pkt3.packet.signature = 1;
+	pkt4.packet.signature = 1;
+
+	int sig_of_pkt1;
+	int sig_of_pkt2;
+	int sig_of_pkt3;
+	int sig_of_pkt4;
+
+	klee_make_symbolic(&sig_of_pkt1, sizeof(sig_of_pkt1), "pkt1.packet.signature");
+	klee_make_symbolic(&sig_of_pkt2, sizeof(sig_of_pkt2), "pkt2.packet.signature");
+	klee_make_symbolic(&sig_of_pkt3, sizeof(sig_of_pkt3), "pkt3.packet.signature");
+	klee_make_symbolic(&sig_of_pkt4, sizeof(sig_of_pkt4), "pkt4.packet.signature");
+
+	memcpy(&pkt1.packet.signature, &sig_of_pkt1, sizeof(sig_of_pkt1));
+	memcpy(&pkt2.packet.signature, &sig_of_pkt2, sizeof(sig_of_pkt2));
+	memcpy(&pkt3.packet.signature, &sig_of_pkt3, sizeof(sig_of_pkt3));
+	memcpy(&pkt4.packet.signature, &sig_of_pkt4, sizeof(sig_of_pkt4));
 
 
 /*
